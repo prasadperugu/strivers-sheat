58b8fe0747ad3c93287aa3ee109315149c88fb49
prasadperugu <peruguprasad0@gmail.com>
2023-01-30 10:28:57 +0530

    dsa

---
+++ StriversChatSheet/src/com/prasad/StringComparsion.java
@@ -0,0 +1,31 @@
+package com.prasad;
+
+public class StringComparsion {
+    public static void main(String[] args) {
+        /*
+        comparsion of strings
+        1. == (comprator) method
+        checks reference variables(same values) are pointing to same object or not in pool;
+        2. .equals() method
+        when you only need to check the values.
+
+         */
+
+        String a = "prasad";
+        String b = "prasad";
+        System.out.println(a==b);
+
+
+        /*
+         create different objects of same value:
+         these values are created outside the pool but inside the heap
+         */
+
+         String  c = new String("prasad");
+         String  d = new String("prasad");
+        System.out.println(c==d); // false
+        System.out.println(c.equals(d)); //true
+
+
+    }
+}
